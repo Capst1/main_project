0033d22e1bab58ff731db544edc739ee6e2c1417
kimday0326 <ktnqkr01@gmail.com>
2024-11-12 16:31:32 +0900

    test: 캐싱 테스트 작성

--- src/test/java/dev/capstone/capstone_server/SubjectServiceCacheTest.java
+++ src/test/java/dev/capstone/capstone_server/SubjectServiceCacheTest.java
@@ -2,15 +2,16 @@ package dev.capstone.capstone_server;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 import dev.capstone.capstone_server.service.SubjectService;
 
-@EnableCaching
 @ActiveProfiles("test")
 @SpringBootTest(classes = TestConfig.class)
 public class SubjectServiceCacheTest {
@@ -18,19 +19,28 @@ public class SubjectServiceCacheTest {
 	@Autowired
 	private SubjectService subjectService;
 
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
+
+	@BeforeEach
+	public void clearCache() {
+		// Redis 캐시 초기화
+		redisTemplate.getConnectionFactory().getConnection().flushDb();
+	}
+
 	@Test
+	@DisplayName("레디스 캐싱 테스트")
 	public void testCacheable() {
 		// 첫 번째 호출: 캐시되지 않은 상태이므로 3초 지연 발생
 		long start = System.currentTimeMillis();
-		subjectService.getAllSubjects();
+		subjectService.getSubjectById(2019100000L);
 		long firstCallDuration = System.currentTimeMillis() - start;
 
 		// 두 번째 호출: 캐시된 결과 반환이므로 지연 없음
 		start = System.currentTimeMillis();
-		subjectService.getAllSubjects();
+		subjectService.getSubjectById(2019100000L);
 		long secondCallDuration = System.currentTimeMillis() - start;
-		System.out.println(secondCallDuration);
 		assertTrue(firstCallDuration > 3000);
-		assertTrue(secondCallDuration < 200); // 캐시된 응답은 바로 반환
+		assertTrue(secondCallDuration < 100); // 캐시된 응답은 바로 반환
 	}
 }
--- src/test/java/dev/capstone/capstone_server/TestConfig.java
+++ src/test/java/dev/capstone/capstone_server/TestConfig.java
@@ -1,15 +1,26 @@
 package dev.capstone.capstone_server;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import dev.capstone.capstone_server.repository.FakeSubjectRepository;
 import dev.capstone.capstone_server.repository.SubjectRepository;
 
+@EnableCaching
 @TestConfiguration
 public class TestConfig {
 	@Bean
 	public SubjectRepository subjectRepository() {
 		return new FakeSubjectRepository();
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		return template;
 	}
 }
